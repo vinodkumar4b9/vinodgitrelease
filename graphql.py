3237c4d585644c3079684b4645309f3308973eec
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-10-13 15:28:30 +0530

    Create graphql.py

---
+++ graphql.py
@@ -0,0 +1,31 @@
+import requests
+
+headers = {"Authorization": "Bearer YOUR API KEY"}
+
+
+def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+        
+# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
+query = """
+{
+  viewer {
+    login
+  }
+  rateLimit {
+    limit
+    cost
+    remaining
+    resetAt
+  }
+}
+"""
+
+result = run_query(query) # Execute the query
+remaining_rate_limit = result["data"]["rateLimit"]["remaining"] # Drill down the dictionary
+print("Remaining rate limit - {}".format(remaining_rate_limit))
